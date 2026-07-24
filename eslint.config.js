34d98e43b08434f86fed501d836b7f63d1a59ab9
Joseph Loomis <73568630+Joehl1103@users.noreply.github.com>
2026-07-24 15:17:43 -0500

    fixed linting errors

--- eslint.config.js
+++ eslint.config.js
@@ -1,48 +1,48 @@
-const js = require("@eslint/js");
-const react = require("eslint-plugin-react");
-const jest = require("eslint-plugin-jest");
-const globals = require("globals");
+const js = require('@eslint/js')
+const react = require('eslint-plugin-react')
+const jest = require('eslint-plugin-jest')
+const globals = require('globals')
 
 module.exports = [
   {
     ignores: [
-      "webpack.config.js",
-      "eslint.config.js",
-      ".eslintrc.js",
-      "node_modules/**",
-      "dist/**",
+      'webpack.config.js',
+      'eslint.config.js',
+      '.eslintrc.js',
+      'node_modules/**',
+      'dist/**',
     ],
   },
   js.configs.recommended,
   {
-    files: ["app.js"],
+    files: ['app.js'],
     languageOptions: {
       ecmaVersion: 2018,
-      sourceType: "commonjs",
+      sourceType: 'commonjs',
       globals: {
         ...globals.node,
       },
     },
     rules: {
-      indent: ["error", 2],
-      "linebreak-style": ["error", "unix"],
-      quotes: ["error", "single"],
-      semi: ["error", "never"],
-      eqeqeq: "error",
-      "no-trailing-spaces": "error",
-      "object-curly-spacing": ["error", "always"],
-      "arrow-spacing": ["error", { before: true, after: true }],
-      "no-console": 0,
+      indent: ['error', 2],
+      'linebreak-style': ['error', 'unix'],
+      quotes: ['error', 'single'],
+      semi: ['error', 'never'],
+      eqeqeq: 'error',
+      'no-trailing-spaces': 'error',
+      'object-curly-spacing': ['error', 'always'],
+      'arrow-spacing': ['error', { before: true, after: true }],
+      'no-console': 0,
     },
   },
   {
-    files: ["src/**/*.{js,jsx}"],
+    files: ['src/**/*.{js,jsx}'],
     plugins: {
       react,
     },
     languageOptions: {
       ecmaVersion: 2018,
-      sourceType: "module",
+      sourceType: 'module',
       parserOptions: {
         ecmaFeatures: {
           jsx: true,
@@ -55,33 +55,38 @@ module.exports = [
     },
     settings: {
       react: {
-        version: "detect",
+        version: 'detect',
       },
     },
     rules: {
       ...react.configs.recommended.rules,
-      indent: ["error", 2],
-      "linebreak-style": ["error", "unix"],
-      quotes: ["error", "single"],
-      semi: ["error", "never"],
-      eqeqeq: "error",
-      "no-trailing-spaces": "error",
-      "object-curly-spacing": ["error", "always"],
-      "arrow-spacing": ["error", { before: true, after: true }],
-      "no-console": "error",
-      "react/prop-types": 0,
+      indent: ['error', 2],
+      'linebreak-style': ['error', 'unix'],
+      quotes: ['error', 'single'],
+      semi: ['error', 'never'],
+      eqeqeq: 'error',
+      'no-trailing-spaces': 'error',
+      'object-curly-spacing': ['error', 'always'],
+      'arrow-spacing': ['error', { before: true, after: true }],
+      'no-console': 'error',
+      'react/prop-types': 0,
     },
   },
   {
-    files: ["./jest.setup.js", "test/**/*.{js,jsx}"],
+    files: ['./jest.setup.js', 'test/**/*.{js,jsx}'],
     plugins: {
       jest,
     },
     languageOptions: {
+      parserOptions: {
+        ecmaFeatures: {
+          jsx: true,
+        },
+      },
       globals: {
         ...globals.node,
         ...globals.jest,
       },
     },
   },
-];
+]
