45cadc91ab19c8ea6752677ae6858178e2a9ae20
Joseph Loomis <73568630+Joehl1103@users.noreply.github.com>
2026-08-02 16:29:36 -0500

    added eslint ignore rule for simulation

--- app.js
+++ app.js
@@ -16,6 +16,7 @@ app.get('/version', (req, res) => {
 })
 
 app.get('/health', (req, res) => {
+  // eslint-disable-next-line no-constant-condition
   if (true) throw 'Error...'
   res.send('ok')
 })
